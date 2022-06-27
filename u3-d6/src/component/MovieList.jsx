dbe47b322a222f83f040de2efb5da6999b9c94f4
marios <mgeorgak859@gmail.com>
2022-06-27 17:24:24 +0300

    fixed syntax bug

--- u3-d6/src/component/MovieList.jsx
+++ u3-d6/src/component/MovieList.jsx
@@ -1,45 +1,46 @@
 import React, { Component } from 'react'
-import {Card, Spinner} from 'react-bootstrap'
+import { Card, Spinner } from 'react-bootstrap'
 
 export default class MovieList extends Component {
-    state = {
-        movies: null
+  state = {
+    movies: null
+  }
+  componentDidMount = () => {
+    this.fetchMovies()
+  }
+  fetchMovies = async () => {
+    try {
+      let response = await fetch('http://www.omdbapi.com/?apikey=24ad60e9&s=Batman')
+      let data = await response.json()
+      this.setState({ movies: data.Search })
+      console.log(data.Search)
     }
-    componentDidMount = () => {
-      this.fetchMovies()
+    catch (err) {
+      console.log(err)
     }
-    fetchMovies = async () => {
-        try{
-let response = await fetch('http://www.omdbapi.com/?apikey=24ad60e9&s=Batman')
-let data = await response.json()
-this.setState({movies: data})
-console.log(data)
-        }
-        catch(err) {
-console.log(err)
-        }
-    }
+  }
   render() {
     return (
       <>
-      {this.state.movies ? (
-        <Card>
-          <Card.Img
-            variant="top"
-            src={this.state.movies.Poster}
-          />
-          <Card.Body className="text-dark">
-            <Card.Title>{this.state.movies.Title}</Card.Title>
-            <Card.Text>
-              {this.state.movies.Year} -{' '}
-              {this.state.movies.imdbID}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ) : (
-        <Spinner variant="warning" animation="border" />
-      )}
-    </>
+        {this.state.movies ? (
+          this.state.movies.map((element)=>(<Card key={element.imdbID}>
+            <Card.Img
+              variant="top"
+              src={element.Poster}
+            />
+            <Card.Body className="text-dark">
+              <Card.Title>{element.Title}</Card.Title>
+              <Card.Text>
+                {element.Year} -{' '}
+                {element.imdbID}
+              </Card.Text>
+            </Card.Body>
+          </Card>))
+          
+        ) : (
+          <Spinner variant="warning" animation="border" />
+        )}
+      </>
     )
   }
 }
