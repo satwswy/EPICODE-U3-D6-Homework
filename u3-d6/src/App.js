e12c262bc4c06f2aa1393523dfb55cb5dcafb004
marios <mgeorgak859@gmail.com>
2022-06-27 15:33:11 +0300

    updated app.js as class component and deleted import of logo

--- u3-d6/src/App.js
+++ u3-d6/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <div className="App"></div>
+    );
+  }
 }
 
 export default App;
