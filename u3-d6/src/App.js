3592d7f1b11b565f5c97fc25b26b218e68f9eed7
marios <mgeorgak859@gmail.com>
2022-06-27 15:39:39 +0300

    Update App.js

--- u3-d6/src/App.js
+++ u3-d6/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Component } from "react";
+import {Container, Row, Col} from "react-bootstrap"
 
 class App extends Component {
   render() {
     return (
-    <div className="App"></div>
+    <div className="App">
+      <Container>
+          <Row className="justify-content-center mt-3 mb-2">
+            <Col md={6}>
+              
+            </Col>
+          </Row>
+       </Container>   
+    </div>
     );
   }
 }
